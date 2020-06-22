6f303c499203863c87e04a8c2e8068b1cf711b52
Thomas Vanhuffel <thomasvanhuffel@hotmail.com>
2020-06-22 15:24:39 +0200

    views

--- ProjectRepo/Controllers/PortfolioController.cs
+++ ProjectRepo/Controllers/PortfolioController.cs
@@ -15,91 +15,46 @@ namespace PortfolioApp.Controllers
 {
     public class PortfolioController : Controller
     {
-
         private readonly PortfolioDb _projectDb;
 
-
-
         public PortfolioController(PortfolioDb portfolioDb)
-
         {
-
             _projectDb = portfolioDb;
-
         }
 
-
-
         public async Task<IActionResult> Index()
-
         {
-
             List<PortfolioListViewModel> vmList = new List<PortfolioListViewModel>();
-
             var portfolios = await _projectDb.Portfolio.Include(x => x.Status).Include(x => x.PortfolioTags).ThenInclude(x => x.Tag).ToListAsync();
-
             foreach (var portfolio in portfolios)
-
             {
-
                 PortfolioListViewModel vm = new PortfolioListViewModel()
-
                 {
-
                     Id = portfolio.Id,
-
                     CompletionDate = portfolio.CompletionDate,
-
                     Status = portfolio.Status.Description,
-
                     Tags = portfolio.PortfolioTags.Select(x => x.Tag.Description).ToList(),
-
                     PhotoUrl = portfolio.PhotoUrl,
-
                     Title = portfolio.Title
-
                 };
-
                 vmList.Add(vm);
-
             }
-
-
-
             return View(vmList);
-
         }
 
         public async Task<IActionResult> Detail(int id)
-
         {
-
             Portfolio portfolio = await _projectDb.Portfolio.Include(x => x.Status).Include(x => x.PortfolioTags).ThenInclude(x => x.Tag).FirstOrDefaultAsync(x => x.Id == id);
-
             PortfolioDetailViewModel vm = new PortfolioDetailViewModel()
-
             {
-
                 Title = portfolio.Title,
-
                 CompletionDate = portfolio.CompletionDate,
-
                 Status = portfolio.Status.Description,
-
                 Tags = portfolio.PortfolioTags.Select(x => x.Tag.Description).ToList(),
-
                 PhotoUrl = portfolio.PhotoUrl,
-
                 Description = portfolio.Description
-
             };
-
             return View(vm);
-
         }
-
-
-
     }
-
 }
